4319ed25fef888d61851400880e84ed29bdf1fad
Theo Dgl <dimeglio.theo@gmail.com>
2023-12-21 20:12:13 -0500

    validate register api endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,13 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
  
-Route::post('/tokens', function (Request $request) {
-    $token = $request->user()->createToken($request->token_name);
- 
-    return ['token' => $token->plainTextToken];
-});
+Route::post('/register', [RegisterController::class, 'register']);
+Route::post('/login', [LoginController::class, 'login']);
 
 Route::get('/posts', [PostController::class,'get']);
 
